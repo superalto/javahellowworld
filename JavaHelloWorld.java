d1580341577a0b2fd0d262e71096509b06a14b51
superalto <84994520+superalto@users.noreply.github.com>
2021-06-28 02:20:08 -0700

    Add files via upload

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,7 +3,7 @@ public class JavaHelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("Java Hello World");
-		System.out.println("Hello again")
+		System.out.println("Hello again");
 		System.out.println("Hello three times")
 	}
 }
